357d9106b5c990197200dfe30860ba8b5b6c532e
anonymousomeone <leoistheworst22@gmail.com>
2022-04-11 22:15:08 -0700

    among us??

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,9 +40,9 @@ subprojects {
 
     // Fill out with your info
     aliucord {
-        author("DISCORD USERNAME", 123456789L)
-        updateUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/%s.zip")
+        author("Zalander", 527196921513902090)
+        updateUrl.set("https://raw.githubusercontent.com/Zalander0/Aliucord-Plugins/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/Zalander0/Aliucord-Plugins/builds/%s.zip")
     }
 
     android {
---
+++ lmgtfy/src/main/kotlin/com/github/zalandr/MyFirstCommand.kt
@@ -0,0 +1,35 @@
+package com.github.zalandr
+
+import android.content.Context
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.api.CommandsAPI
+import com.aliucord.entities.Plugin
+import com.discord.api.commands.ApplicationCommandType
+
+// Aliucord Plugin annotation. Must be present on the main class of your plugin
+@AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
+// Plugin class. Must extend Plugin and override start and stop
+// Learn more: https://github.com/Aliucord/documentation/blob/main/plugin-dev/1_introduction.md#basic-plugin-structure
+class MyFirstCommand : Plugin() {
+    override fun start(context: Context) {
+        // A bit more advanced command with arguments
+        commands.registerCommand("lmgtfy", "Here, let me google that for you.",
+            Utils.createCommandOption(ApplicationCommandType.STRING, "query", "what you want them to google i guess", required = true)
+        ) { ctx ->
+            // Check if a user argument was passed
+            if (ctx.containsArg("query")) {
+                val query = ctx.getStringOrDefault("query")
+                CommandsAPI.CommandResult(Regex("\s").replace(query, "+"), null, true)
+            } else {
+                // you did a bad
+                CommandsAPI.CommandResult("you did a bad", null, false)
+            }
+        }
+    }
+
+    override fun stop(context: Context) {
+        // Unregister our commands
+        commands.unregisterAll()
+    }
+}
