157e34b8e0a831cbebd1f12933c0b8bd663da249
Zalander0 <103299145+Zalander0@users.noreply.github.com>
2022-04-13 14:04:57 -0700

    2doom :husk:

--- GroupchatInvite/src/main/kotlin/com/github/zalandr/gcinvite.kt
+++ GroupchatInvite/src/main/kotlin/com/github/zalandr/gcinvite.kt
@@ -7,6 +7,7 @@ import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
 import com.discord.api.commands.ApplicationCommandType
 import com.aliucord.Http
+import org.json.JSONObject
 
 // Aliucord Plugin annotation. Must be present on the main class of your plugin
 @AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
@@ -23,8 +24,8 @@ class GCInvite : Plugin() {
             // group channel is type 3 fyi
             if (it.currentChannel.type == 3) {
                 val send = it.getBoolOrDefault("send", false);
-                val invite = Http.Request.newDiscordRequest("/v9/channels/@me/${it.channelId}/invites");
-                    .executeWithJson("{max_age: 86400}");
+                val invite = Http.Request.newDiscordRequest("/v9/channels/@me/${it.channelId}/invites")
+                    .executeWithJson(JSONObject("{max_age: 86400}"));
                 CommandsAPI.CommandResult(invite, send);
             } else {
                 CommandsAPI.CommandResult("this isnt a groupchat channel (you did a bad)", false);
