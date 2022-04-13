d3a96532c765500e10a8e9aa03e1771469a1f395
Zalander0 <103299145+Zalander0@users.noreply.github.com>
2022-04-13 13:26:26 -0700

    initial commit (realistic???)

---
+++ GroupchatInvite/src/main/kotlin/com/github/zalandr/gcinvite.kt
@@ -0,0 +1,37 @@
+package com.github.zalandr
+
+import android.content.Context
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.api.CommandsAPI
+import com.aliucord.entities.Plugin
+import com.discord.api.commands.ApplicationCommandType
+import com.aliucord.Http
+
+// Aliucord Plugin annotation. Must be present on the main class of your plugin
+@AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
+// Plugin class. Must extend Plugin and override start and stop
+// Learn more: https://github.com/Aliucord/documentation/blob/main/plugin-dev/1_introduction.md#basic-plugin-structure
+class GCInvite : Plugin() {
+    override fun start(context: Context) {
+        // A bit more advanced command with arguments
+        commands.registerCommand(
+            "invite", "get groupchat invite link", 
+            Utils.createCommandOption(ApplicationCommandType.STRING, "send", "whether to send the link - default: false")
+        ){
+            if (it.currentChannel.type == 'group') {
+                val send = it.getBoolOrDefault("send", false);
+                val invite = Http.Request.newDiscordRequest("/v9/channels/${it.channelId}/invites")
+                    .executeWithJson();
+                CommandsAPI.CommandResult(invite, send)
+            } else {
+                CommandsAPI.CommandResult("this isnt a groupchat channel (you did a bad)", false)
+            }
+        }
+    }
+
+    override fun stop(context: Context) {
+        // Unregister our commands
+        commands.unregisterAll()
+    }
+}
